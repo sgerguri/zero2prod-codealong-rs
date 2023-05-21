fe9bf467dfa650c9cf1664d887e187ef86537922
Shkodran Gerguri <gerguris@gmail.com>
2023-05-21 01:53:39 +0100

    add health check and tests

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::net::TcpListener;
+
+use actix_web::{web, App, HttpResponse, HttpServer, HttpRequest, Responder, dev::Server};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello, {}!", &name)
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
+        })
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
