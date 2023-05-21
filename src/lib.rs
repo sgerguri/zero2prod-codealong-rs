a2edddea0236a820dd517b0bd7df4893c9b2d02e
Shkodran Gerguri <gerguris@gmail.com>
2023-05-21 21:02:21 +0100

    Add local DB setup and subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
-
-use actix_web::{web, App, HttpResponse, HttpServer, HttpRequest, Responder, dev::Server};
+use serde::Deserialize;
+use actix_web::{web::{self, Form}, App, HttpResponse, HttpServer, HttpRequest, Responder, dev::Server};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -11,12 +11,24 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+
+#[derive(Deserialize)]
+struct SubDetails{
+    name: String,
+    email: String
+} 
+
+async fn subscribe(form: Form<SubDetails>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
         })
         .listen(listener)?
         .run();
